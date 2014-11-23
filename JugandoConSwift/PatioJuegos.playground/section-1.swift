49d89cf629fc452e71be23778f675362c5ab8557
Erick Chali <erickdemon@gmail.com>
2014-11-23 00:55:30 -0600

    Basics
    Des comentar

---
+++ JugandoConSwift/PatioJuegos.playground/section-1.swift
@@ -0,0 +1,46 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+
+//var name = "Erick"
+//var edad = 21
+//var pi = 3.141516
+//
+
+
+/**Arreglos**/
+//var notas = [75,80,100,95]
+//notas.append(78)
+//
+//var agenda = ["Yo":"30141757","Papa":"12345678","Sister":"87654321"]
+//var miNumero = agenda["Yo"]
+//
+//agenda["Tia"]="24388583"
+//agenda
+//
+//notas.removeAtIndex(3)
+//notas
+//println(notas)
+//
+//agenda["Papa"]=nil
+//println(agenda)
+//
+//var cantidadNotas = notas.count
+//var promedio = (notas[0] + notas[1] + notas[2] + notas[3])/4
+//println(promedio)
+//
+//var arregloEnteros = [Int]()
+//var arregloStrings = [String]()
+//var listaNueva = Dictionary<String,Float>()
+//listaNueva["Decimal"] = 2.3
+//println(listaNueva)
+
+
+/**Diccionarios**/
+//var colores = ["Yo":"Blanco","Jimena":"Negro","Amiga":"Azul"]
+//println("Hay \(colores.count), el color favorito de Jimena es: " + colores["Jimena"]! + "Mi color favorito es " + colores["Yo"]!)
+
+
+/**Ciclos IF**/
+//igual que java
